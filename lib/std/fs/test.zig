c5656728e02cfa7942c015f01dcff9c263a1fb06
Pat Tullmann <pat.github@tullmann.org>
2023-10-23 15:23:19 -0700

    std/fs/test.zig: Remove work-around for stat() failures on glibc
    glibc variants now support the stat-family of calls correctly, so
    this test is safe to include.

--- lib/std/fs/test.zig
+++ lib/std/fs/test.zig
@@ -582,8 +582,7 @@ test "readAllAlloc" {
 }
 
 test "Dir.statFile" {
-    // TODO: Re-enable once https://github.com/ziglang/zig/issues/17034 is solved
-    if (builtin.os.tag == .linux and builtin.link_libc and builtin.abi == .gnu) return error.SkipZigTest;
+    if (builtin.zig_backend == .stage2_x86_64) return error.SkipZigTest;
 
     try testWithAllSupportedPathTypes(struct {
         fn impl(ctx: *TestContext) !void {
